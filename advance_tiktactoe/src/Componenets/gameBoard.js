a5f67041beca842d21e96ab924cd4ed6392d62f8
Prasun <champgod12@gmail.com>
2024-05-11 14:44:15 +0545

    Added UI elements
    Added circle, cross, winning line, winning cricle and winning cross

--- advance_tiktactoe/src/Componenets/gameBoard.js
+++ advance_tiktactoe/src/Componenets/gameBoard.js
@@ -5,132 +5,187 @@ export default function GameBoard() {
   return (
     <div className='gameBoardContainer'>
       <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
         </div>
+        <div className="winningCross hide "></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
+      </div>
+      <div className="smallerGameContainer">
+        <div className='smallGameBoard '>
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
+        </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide"></div>
       </div>
       <div className="smallerGameContainer">
         <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
+          <div className="playingBox">
+            <div className="circle"></div>          
+          </div>
+          <div className="playingBox">
+            <div className="cross"></div>          
+          </div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>  
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="playingBox"></div>
+          <div className="winningLine hide"></div>
         </div>
+        <div className="winningCross hide hide"></div>
+        <div className="winningCircle hide hide"></div>
       </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
 
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-      <div className="smallerGameContainer">
-        <div className='smallGameBoard'>
-          <div className="playingBox">1</div>
-          <div className="playingBox">2</div>
-          <div className="playingBox">3</div>
-          <div className="playingBox">4</div>
-          <div className="playingBox">5</div>
-          <div className="playingBox">6</div>
-          <div className="playingBox">7</div>
-          <div className="playingBox">8</div>
-          <div className="playingBox">9</div>
-
-        </div>
-      </div>
-        
+  
 
     </div>
   )
